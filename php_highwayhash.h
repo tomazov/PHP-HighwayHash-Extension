7305cddd035c2fe3050f47c467eac52607fde19a
asyslinux <syslinux@protonmail.com>
2020-12-18 15:03:55 +0300

    PHP 7.2+ Support Fix

--- php_highwayhash.h
+++ php_highwayhash.h
@@ -8,15 +8,15 @@
 #include "config.h"
 #endif 
 
-extern "C" {
-#include "php.h"
-}
-
 extern zend_module_entry highwayhash_module_entry;
 #define phpext_highwayhash_ptr &highwayhash_module_entry;
 
 PHP_FUNCTION(highway_hash);
 PHP_FUNCTION(highway_hash32);
+
+PHP_MINIT_FUNCTION(highwayhash);
+PHP_MSHUTDOWN_FUNCTION(highwayhash);
+PHP_MINFO_FUNCTION(highwayhash);
 
 typedef struct {
         char *input;
